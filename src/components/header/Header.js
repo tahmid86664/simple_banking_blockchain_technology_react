b15c9ee515e2b77d083e341b437505b110c33e97
Tahmid Khandokar <tahmidkhandokar82@gmail.com>
2021-11-29 11:25:35 +0600

    initial setup

---
+++ src/components/header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.scss';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <h1>Simple Banking System with Blockchain Technology 🔥🔥🔥</h1>
+            <a className="headerLink" href="/">Home</a>
+            <a className="headerLink" href="/#">Add transaction</a>
+        </div>
+    )
+}
+
+export default Header;
